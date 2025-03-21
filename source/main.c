eb5c6e5e03248119c136982f5b9383442123f699
acg <ashtongoan@gmail.com>
2025-03-21 15:34:57 -0400

    initial commit

---
+++ source/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef struct vector {
+	double i;
+	double j;
+	double k;
+} vector;
+
+void print_vector(vector v);
+void cross_product(vector v1, vector v2, vector *v1_x_v2);
+void normalize_vector(vector *v);
+void scale_vector(vector *v);
+double vector_magnitude(vector v);
+
+int main()
+{
+	vector my_vector;
+	my_vector.i = 1.5;
+	my_vector.j = 2.0;
+	my_vector.k = 0.0;
+	
+	vector my_vector2;
+	my_vector2.i = 0.8;
+	my_vector2.j = 2.7;
+	my_vector2.k = 0.0;
+	
+	vector v1_x_v2;
+	cross_product(my_vector, my_vector2, &v1_x_v2);
+	
+	print_vector(my_vector);
+	print_vector(my_vector2);
+	printf("The cross product of v1 and v2: ");
+	print_vector(v1_x_v2);
+	return 0;	
+}
+
+void print_vector(vector v) {
+	printf("i: %.2f | j: %.2f | k: %.2f\n", v.i, v.j, v.k);
+}
+
+void cross_product(vector v1, vector v2, vector *v1_x_v2)
+{
+	v1_x_v2->i = v1.j*v2.k - v1.k*v2.j;
+	v1_x_v2->j = v1.k*v2.i - v1.i*v2.k;
+	v1_x_v2->k = v1.i*v2.j - v1.j*v2.i;
+	return;
+}
---
+++ source/vector_ops.c
@@ -0,0 +1,5 @@
+#include "vector_ops.h"
+void create_vector(double i, double j, double k)
+{
+	
+}
