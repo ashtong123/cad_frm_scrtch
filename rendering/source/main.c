cc735e4b6990d5b553cee8933016ef1d637af1d4
Goan <E40088284@adxuser.com>
2025-03-27 16:30:59 -0400

    added some more functionality to rendering module

--- rendering/source/main.c
+++ rendering/source/main.c
@@ -3,11 +3,14 @@
 #include <GLFW/glfw3.h>
 
 void error_callback(int error, const char* description);
+void close_window_callback(GLFWwindow* window);
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main()
 {
 	//setup error callback
 	glfwSetErrorCallback(error_callback);
+	
 	//initialize glfw
 	if (!glfwInit()) {
 		printf("Failed to initialize glfw\n");// Initialization failed
@@ -15,18 +18,35 @@ int main()
 	else {
 		printf("glfw initializated successfully\n");
 	}
+	
 	//create window
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 	if (!window)
 	{
 		printf("Failed to open window\n");
 	}
-	//keep window open until prompted by user to close
-	printf("Press enter to close window\n");
-	while(!getchar());
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	
+	glfwSetWindowCloseCallback(window, close_window_callback);
+	glfwSetKeyCallback(window, key_callback);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		// Keep running
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	
 	//terminate glfw session
 	glfwTerminate();
 	printf("glfw terminated successfully\n");
+	//keep terminal open until prompted by user to close
+	printf("Press ENTER to close program");
+	while(!getchar());
 	
 	return 0;
 }
@@ -34,4 +54,17 @@ int main()
 void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
+}
+
+void close_window_callback(GLFWwindow* window)
+{
+	printf("Window closed by EXIT button click\n");
+	return;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+	printf("Window closed by ESC key press\n");
 }
