1c28ccfb2408f3c39fd87065912b1e75a568121c
Goan <E40088284@adxuser.com>
2025-03-27 15:21:40 -0400

    create window added to /rendering

--- rendering/source/main.c
+++ rendering/source/main.c
@@ -2,13 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description);
+
 int main()
 {
-	if (!glfwInit())
-	{
+	//setup error callback
+	glfwSetErrorCallback(error_callback);
+	//initialize glfw
+	if (!glfwInit()) {
 		printf("Failed to initialize glfw\n");// Initialization failed
 	}
+	else {
+		printf("glfw initializated successfully\n");
+	}
+	//create window
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+	{
+		printf("Failed to open window\n");
+	}
+	//keep window open until prompted by user to close
+	printf("Press enter to close window\n");
+	while(!getchar());
+	//terminate glfw session
+	glfwTerminate();
+	printf("glfw terminated successfully\n");
 	
-	printf("glfw initializated successfully\n");
 	return 0;
+}
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
 }
